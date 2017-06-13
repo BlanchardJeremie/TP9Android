4944d0020d34465841d40a5d0535c43d6a9889f8
Blanchard Jérémie <jeremie.blanchard@outlook.fr>
2017-06-13 15:51:05 +0200

    classe Point v2

--- app/src/main/java/com/example/admin/tp9android/Point.java
+++ app/src/main/java/com/example/admin/tp9android/Point.java
@@ -14,13 +14,11 @@ public class Point implements Parcelable{
         return 0;
     }
 
-    public void writeToParcel(Parcel out,int flags) {
-        out.writeInt(compteur);
-    }
+    public void writeToParcel(Parcel out,int flags) { out.writeInt(compteur);}
 
-    public static final Parcelable.Creator<Point> CREATOR = new Parcelable().Creator<Point>() {
-        public Point createFromParcel(Parcel "in");{
-            return new Point("in");
+    public static final Parcelable.Creator<Point> CREATOR = new Parcelable.Creator<Point>() {
+        public Point createFromParcel(Parcel in){
+            return new Point(in);
         }
         public Point[] newArray(int size) {
             return new Point[size];
